91e2ab167eeb322a2aa5565cf3f59902eb060d83
Akshita-2004 <2210030358cse@gmail.com>
2025-07-12 14:27:01 +0530

    Add files via upload

---
+++ week4/Create a Spring Web Project using Maven/country-demo (1)/hellorest/src/main/java/com/akshita/hellorest/HelloController.java
@@ -0,0 +1,14 @@
+package com.akshita.hellorest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
+s
